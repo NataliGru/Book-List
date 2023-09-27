0bab6ffdb6bdd8b411ab4b4cebdb636d3e8b802b
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-09-27 19:44:48 +0300

    added better way to get category

--- generateData.js
+++ generateData.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+const categories = ["Fiction", "Science Fiction", "Mystery", "Romance", "Fantasy", "Thriller", "Non-Fiction", "Biography", "Self-Help", "Cooking"];
+
 let currentISBN = 1;
 const generateISBN = () => {
   const ISBN = `${currentISBN}`.padStart(10, '0');
@@ -9,11 +11,13 @@ const generateISBN = () => {
 
 const data = { books: [] };
 for (let i = 1; i <= 99; i++) {
+  const categoryIndex = Math.floor((i - 1) / 10);
+  const category = categories[categoryIndex];
   data.books.push({
     id: i,
     title: `Book ${i}`,
     author: `Author ${i}`,
-    category: `Category ${i}`,
+    category,
     isbn: generateISBN(),
     imgUrl: `https://via.placeholder.com/150/92c952?text=Book${i}`,
     createdAt: new Date().toLocaleString(),
